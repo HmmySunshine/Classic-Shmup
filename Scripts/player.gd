dd6fcb7881cb0846e393a809f9b8042083257bfb
hide on bush <75515576+HmmySunshine@users.noreply.github.com>
2025-05-12 14:18:06 +0800

    发射子弹

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,22 @@
 extends Area2D
 
+var canShoot: bool = true
 @export var speed: float = 150
+@export var coolDown = 0.25
+@export var bulletScene: PackedScene
+
 @onready var screenSize = get_viewport_rect().size
 @onready var clampMinSize = Vector2(8, 8)
 @onready var clampMaxSize = screenSize - clampMinSize
+@onready var startPosition: Vector2 = Vector2(screenSize.x / 2, screenSize.y - 64)
+
+
+func _ready() -> void:
+	Start()
+	
+func Start():
+	position = startPosition
+	$GunCooldown.wait_time = 0.25
 
 func _process(delta: float) -> void:
 	var input = GetDirection()
@@ -17,6 +30,20 @@ func _process(delta: float) -> void:
 		$Ship.frame = 1
 		$Ship/Boosters.animation = "forward"
 	MoveMent(delta, input)
+	if Input.is_action_pressed("shoot"):
+		Shoot()
+
+func Shoot():
+	if not canShoot:
+		return
+	canShoot = false
+	$GunCooldown.start()
+	SpawnBullet()
+	
+func SpawnBullet():
+	var bullet = bulletScene.instantiate()
+	get_tree().root.add_child(bullet)
+	bullet.Start($BulletPosition.global_position)
 
 func GetDirection():
 	return Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -30,3 +57,7 @@ func MoveLeft(dirX: float):
 func MoveMent(delta: float, direction: Vector2):
 	position += speed * delta * direction
 	position = position.clamp(clampMinSize, clampMaxSize)
+
+
+func _on_gun_cooldown_timeout() -> void:
+	canShoot = true
